d0780ed76ccf5959d78774e2dde31f67b5c34147
Robert Rothenberg <rrwo@cpan.org>
2018-11-30 01:30:54 +0000

    Commit Changes and bump $VERSION

--- lib/Plack/Middleware/Statsd.pm
+++ lib/Plack/Middleware/Statsd.pm
@@ -17,7 +17,7 @@ use Plack::Util::Accessor qw/ client sample_rate /;
 use Time::HiRes;
 use Try::Tiny;
 
-our $VERSION = 'v0.3.8';
+our $VERSION = 'v0.3.9';
 
 sub call {
     my ( $self, $env ) = @_;
