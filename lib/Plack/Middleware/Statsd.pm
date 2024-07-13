f74d17d7cb97ec599ba49a5c3c8aa7ec39dd596b
Robert Rothenberg <rrwo@cpan.org>
2024-07-13 12:47:47 +0100

    Update POD about supported Perl versions

--- lib/Plack/Middleware/Statsd.pm
+++ lib/Plack/Middleware/Statsd.pm
@@ -481,13 +481,9 @@ need to add metrics logging yourself.
 
 =head1 SUPPORT FOR OLDER PERL VERSIONS
 
-Since v0.6.0, the this module requires Perl v5.14 or later.
+Since v0.7.0, the this module requires Perl v5.20 or later.
 
 Future releases may only support Perl versions released in the last ten years.
-
-If you need this module on Perl v5.10, please use one of the v0.5.x
-versions of this module.  Significant bug or security fixes may be
-backported to those versions.
 
 =head1 SEE ALSO
 
