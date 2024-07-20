46c0d06bbff6d60714367197dde61ffcec89e41f
Robert Rothenberg <rrwo@cpan.org>
2024-07-20 12:52:37 +0100

    Commit Changes and bump $VERSION

--- lib/Plack/Middleware/Statsd.pm
+++ lib/Plack/Middleware/Statsd.pm
@@ -22,7 +22,7 @@ use Try::Tiny;
 
 use experimental qw/ postderef signatures /;
 
-our $VERSION = 'v0.7.1';
+our $VERSION = 'v0.7.2';
 
 # Note: You may be able to omit the client if there is a client
 # defined in the environment hash at C<psgix.monitor.statsd>, and the
