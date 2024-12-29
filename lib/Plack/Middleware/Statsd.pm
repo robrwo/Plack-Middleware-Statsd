2172e78ddea0c2ff8c4a54c045c84c71a2dc89f0
Robert Rothenberg <rrwo@cpan.org>
2024-12-29 17:19:41 +0000

    Add security policy

--- lib/Plack/Middleware/Statsd.pm
+++ lib/Plack/Middleware/Statsd.pm
@@ -498,6 +498,13 @@ L<PSGI>
 The initial development of this module was sponsored by Science Photo
 Library L<https://www.sciencephoto.com>.
 
+=head1 append:BUGS
+
+=head2 Reporting Security Vulnerabilities
+
+Security issues should not be reported on the bugtracker website. Please see F<SECURITY.md> for instructions how to
+report security vulnerabilities
+
 =cut
 
 1;
